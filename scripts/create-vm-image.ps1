0beedf3a30cc25b28499e2b48137bd296f20aca5
Richard Swart <richard.swart@ortec.com>
2022-04-06 14:48:30 +0200

    add image creation

--- scripts/create-vm-image.ps1
+++ scripts/create-vm-image.ps1
@@ -10,27 +10,43 @@ param (
 
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
+    [string] $ResourceGroup,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
     [string] $ImageDisplayName,
 
     [Parameter(Mandatory = $false)]
     [ValidateNotNullOrEmpty()]
-    [string] $ImageQuery = "latest"
+    [string] $Sku = "Premium_LRS",
+
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $DateQuery = "latest"
 )
 
 
 
-# Determin image name and blob uri
-if ($ImageQuery.Equals("latest")) {
+# Determine image name and blob uri
+if ($DateQuery.Equals("latest")) {
     $ImageBlob = (az storage blob list --account-name $StorageAccount -c $ContainerName --query "[?contains(name,'$ImageDisplayName')].{name:name, time:properties.creationTime}" | ConvertFrom-JSON | Sort-Object -Property time | Select-Object -Last 1)
 } else {
-    $ImageBlob = (az storage blob list --account-name $StorageAccount -c $ContainerName --query "[?contains(name,'$ImageDisplayName')&&contains(properties.creationTime,'$ImageQuery')].{name:name, time:properties.creationTime}" | ConvertFrom-JSON | Sort-Object -Property time | Select-Object -Last 1)
+    $ImageBlob = (az storage blob list --account-name $StorageAccount -c $ContainerName --query "[?contains(name,'$ImageDisplayName')&&contains(properties.creationTime,'$DateQuery')].{name:name, time:properties.creationTime}" | ConvertFrom-JSON | Sort-Object -Property time | Select-Object -Last 1)
 }
-az storage blob list --account-name h04p1winstonpbasa01ado -c system --query "[?contains(properties.creationTime,'2022-02-01')].{name:name, time:properties.creationTime}"
 
 $ImageBlobUri='https://{0}.blob.core.windows.net/{1}/{2}' -f $StorageAccount, $ContainerName, $ImageBlob.name
 $ImageName = 'DevOpsImage-{0}-{1}' -f $ImageDisplayName, $ImageBlob.time.toString("yyyy-mm-dd")
 
-Write-Output "##vso[task.setvariable variable=ImageBlobUri;isOutput=true]$ImageBlobUri"
-Write-Output "Stored value: $ImageBlobUri as ImageBlobUri"
-write-Output "##vso[task.setvariable variable=ImageName;isOutput=true]$ImageName"
-Write-Output "Stored value: $ImageName as ImageName"
+# Create the vm image
+az image create --name $ImageName `
+                --resource-group $ResourceGroup `
+                --source $ImageBlobUri `
+                --os-type Linux `
+                --os-disk-caching ReadWrite `
+                --storage-sku $Sku `
+                --location westeurope
+
+# TODO: cleanup old images
+
+
+
