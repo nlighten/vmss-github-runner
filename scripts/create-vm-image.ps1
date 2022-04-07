c354687b71766a43971c9107b51c341c512fa246
Richard <richard.swart@nlighten.nl>
2022-04-07 10:53:37 +0200

    Initial setup (#2)
    * inital
    
    * inital
    
    * inital
    
    * inital
    
    * inital
    
    * inital
    
    * inital
    
    * inital
    
    * add image creation
    
    * add image creation
    
    * add image creation
    
    * add image creation
    
    * add image creation
    
    * add image creation
    
    * create vmss
    
    * create vmss
    
    * create vmss
    
    * create vmss
    
    * create vmss
    
    * create vmss
    
    * create vmss
    
    * create vmss
    
    * create vmss
    
    * create vmss
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * add cloud-config
    
    * cleanup
    
    Co-authored-by: Richard Swart <richard.swart@ortec.com>

---
+++ scripts/create-vm-image.ps1
@@ -0,0 +1,52 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $StorageAccount,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $ContainerName,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $ResourceGroup,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $ImageDisplayName,
+
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Sku = "Premium_LRS",
+
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $DateQuery = "latest"
+)
+
+
+
+# Determine image name and blob uri
+if ($DateQuery.Equals("latest")) {
+    $ImageBlob = (az storage blob list --account-name $StorageAccount -c $ContainerName --query "[?contains(name,'$ImageDisplayName')].{name:name, time:properties.creationTime}" | ConvertFrom-JSON | Sort-Object -Property time | Select-Object -Last 1)
+} else {
+    $ImageBlob = (az storage blob list --account-name $StorageAccount -c $ContainerName --query "[?contains(name,'$ImageDisplayName')&&contains(properties.creationTime,'$DateQuery')].{name:name, time:properties.creationTime}" | ConvertFrom-JSON | Sort-Object -Property time | Select-Object -Last 1)
+}
+
+$ImageBlobUri='https://{0}.blob.core.windows.net/{1}/{2}' -f $StorageAccount, $ContainerName, $ImageBlob.name
+$ImageName = 'DevOpsImage-{0}-{1}' -f $ImageDisplayName, $ImageBlob.time.toString("yyyy-MM-dd")
+
+# Create the vm image
+az image create --name $ImageName `
+                --resource-group $ResourceGroup `
+                --source $ImageBlobUri `
+                --os-type Linux `
+                --os-disk-caching ReadWrite `
+                --storage-sku $Sku `
+                --location westeurope
+
+# TODO: cleanup old images
+
+
+
---
+++ scripts/create-vmss.ps1
@@ -0,0 +1,81 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $VmssName,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $StorageAccount,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Password,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $User,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $VirtualNetworkName,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $VirtualNetworkSubnet,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $NetworkResourceGroup,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $ResourceGroup,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string] $ImageDisplayName,
+
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [string] $DateQuery = "latest"
+)
+
+
+
+# Determine image to use
+if ($DateQuery.Equals("latest")) {
+    $Query = "[?contains(name,'$ImageDisplayName')].{name:name, id:id}"
+} else {
+    $Query = "[?contains(name,'$ImageDisplayName')&&contains(name,'$DateQuery')].{name:name, id:id}"
+}
+$Image = (az image list --resource-group $ResourceGroup --query "$Query" | ConvertFrom-JSON | Sort-Object -Property name | Select-Object -Last 1)
+$SubnetId = az network vnet subnet show --name $VirtualNetworkSubnet --resource-group $NetworkResourceGroup --vnet-name $VirtualNetworkName --query id -o tsv 
+
+
+# Create the scale set
+az vmss create `
+        --resource-group $ResourceGroup `
+        --name $VmssName `
+        --image $Image.id `
+        --admin-username $User `
+        --admin-password $Password `
+        --authentication-type password `
+        --priority Spot `
+        --eviction-policy Delete `
+        --max-price -1 `
+        --instance-count 1 `
+        --custom-data ./config/cloud-config.yaml `
+        --subnet $SubnetId `
+        --load-balancer '""' `
+        --disable-overprovision `
+        --location westeurope
+
+$VmssId = az vmss show --resource-group $ResourceGroup --name $VmssName --query id -o tsv
+$IdentityPrincipalId = az identity create --resource-group $ResourceGroup --name "id-$VmssName" --query principalId -o tsv
+$StorageAccountId = az storage account show --name $StorageAccount --query id -o tsv
+
+az vmss identity assign --resource-group $ResourceGroup  --name $VmssName --identities id-$VmssName 
+
+az role assignment create --role 'Virtual Machine Contributor' --scope $VmssId --assignee-object-id $IdentityPrincipalId
+az role assignment create --role 'Storage Queue Data Contributor' --scope $StorageAccountId --assignee-object-id $IdentityPrincipalId
